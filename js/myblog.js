060a535e2f7ae3668664e016b5781aeca145b061
yz <17771799935@163.com>
2019-11-25 17:11:10 +0800

    191125.3rd

--- js/myblog.js
+++ js/myblog.js
@@ -1,8 +1,24 @@
 $().ready(function(){
+	// 导航栏底部线条
 	$(".nav_ul>li").each(function(){
 		$(this).click(function(){
 			$(".nav_ul>li").removeClass("active");
 			$(this).addClass("active");
 		})
 	})
+	// 导航栏底部线条结束
+	
+	// 汉堡包点击事件
+	$(".hamburger").click(function(){
+		$(".nav").animate({"left":"0"},300);
+		// $(".move_left").removeClass("hide");
+	})
+	// 汉堡包点击事件结束
+	
+	// 侧边栏左移
+	$(".main").click(function(){
+		$(".nav").animate({"left":"-55%"},300);
+		// $(".move_left").addClass("hide");
+	})
+	// 侧边栏左移结束
 })
