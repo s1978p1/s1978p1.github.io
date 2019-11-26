332a6a7e3e39d943783d89cb9f326c7c0d4c1c9b
yz <17771799935@163.com>
2019-11-26 20:22:48 +0800

    191126.3rd

--- js/myblog.js
+++ js/myblog.js
@@ -1,17 +1,17 @@
 //轮播图
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-	spaceBetween: 0,
-	loop: true,
+    slidesPerView: 1,//一次允许展示1张图片
+	spaceBetween: 0,//图片间隔
+	loop: true,//轮播是否循环
 	autoplay: {
 		delay: 2000,//2秒切换一次
 	},
-	pagination: {
+	pagination: {//使用分页器导航
 		el: '.swiper-pagination',
-		clickable: true,
-		dynamicBullets: true,
+		clickable: true,//可点击
+		dynamicBullets: true,//小圆点过多是否隐藏部分
 	},
-	navigation: {
+	navigation: {//使用前进后退按钮
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
