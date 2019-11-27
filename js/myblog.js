3c1c9073883e10575397f741fa18109eac0f27ea
yz <17771799935@163.com>
2019-11-27 14:24:52 +0800

    191127.4th

--- js/myblog.js
+++ js/myblog.js
@@ -1,10 +1,11 @@
 //轮播图
 var swiper = new Swiper('.swiper-container', {
+	speed:1000,
     slidesPerView: 1,//一次允许展示1张图片
 	spaceBetween: 0,//图片间隔
 	loop: true,//轮播是否循环
 	autoplay: {
-		delay: 2000,//2秒切换一次
+		delay: 3000,//3秒切换一次
 		disableOnInteraction:false,
 	},
 	pagination: {//使用分页器导航
